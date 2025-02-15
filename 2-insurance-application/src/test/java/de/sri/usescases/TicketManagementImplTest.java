198542eb7b8dacd134c991a121ac508458540d58
stefanrudi2004 <stefanrudi2004@gmail.com>
2025-02-15 17:03:11 +0100

    test: Change TicketManagementImplTest to integration test

--- 2-insurance-application/src/test/java/de/sri/usescases/TicketManagementImplTest.java
+++ 2-insurance-application/src/test/java/de/sri/usescases/TicketManagementImplTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import de.sri.application.repositories.CustomerRepositoryImpl;
 import de.sri.application.usecases.PolicyManagementImpl;
 import de.sri.application.usecases.TicketManagementImpl;
 import de.sri.domain.entities.Customer;
@@ -20,52 +21,47 @@ import de.sri.domain.entities.PolicyProgram;
 import de.sri.domain.entities.PolicyStatus;
 import de.sri.domain.entities.Ticket;
 import de.sri.domain.repositories.CustomerRepository;
-import de.sri.domain.usecases.CustomerManagement;
-import de.sri.domain.usecases.PolicyManagement;
 import de.sri.domain.valueobjects.Address;
 
-@ExtendWith(MockitoExtension.class)
 public class TicketManagementImplTest {
 
-    @Mock
     private CustomerRepository customerRepository;
-
-    @InjectMocks
     private TicketManagementImpl ticketManagement;
-    
-    @InjectMocks
     private PolicyManagementImpl policyManagementImpl;
 
-    @Mock
-    private PolicyManagement policyManagement;
+    public TicketManagementImplTest() {
+        this.customerRepository = new CustomerRepositoryImpl();
+        this.policyManagementImpl = new PolicyManagementImpl(customerRepository);
+        this.ticketManagement = new TicketManagementImpl(customerRepository, policyManagementImpl);
+    }
 
 
     @Test
     void create_ticket_for_customer() {
-        Customer customer = new Customer(1, "John", "Doe", LocalDate.now().minusYears(60), "john.doe@example.com", new Address("Street", "City", "State", "Zip", "Country"));
-        when(customerRepository.findById(1)).thenReturn(Optional.of(customer));
+        Customer customer = new Customer(100, "John", "Doe", LocalDate.now().minusYears(60), "john.doe@example.com", new Address("Street", "City", "State", "Zip", "Country"));
+        customerRepository.save(customer);
 
-        Ticket ticket = new Ticket(2, LocalDate.now(), 10.0, 1);
-        ticketManagement.createTicketForCustomer(1, ticket);
+        Ticket ticket = new Ticket(2, LocalDate.now(), 10.0, 100);
+        ticketManagement.createTicketForCustomer(100, ticket);
 
-        assertEquals(customerRepository.findById(1).get().getTickets().size(), 1);
-        assertEquals(customerRepository.findById(1).get().getTickets().get(0), ticket);
+        assertEquals(customerRepository.findById(100).get().getTickets().size(), 1);
+        assertEquals(customerRepository.findById(100).get().getTickets().get(0), ticket);
     }
 
     @Test
     void create_ticket_for_customer_increase_policy(){
-        Customer customer = new Customer(1, "John", "Doe", LocalDate.now().minusYears(60), "john.doe@example.com", new Address("Street", "City", "State", "Zip", "Country"));
-        when(customerRepository.findById(1)).thenReturn(Optional.of(customer));
+        Customer customer = new Customer(101, "John", "Doe", LocalDate.now().minusYears(60), "john.doe@example.com", new Address("Street", "City", "State", "Zip", "Country"));
+        customerRepository.save(customer);
 
         Policy policy = new Policy(1, PolicyStatus.ACTIVE, PolicyProgram.BASIC, 10000, 1);
-        policyManagementImpl.addPolicyToCustomer(1, policy);
+        policyManagementImpl.addPolicyToCustomer(101, policy);
 
         double premium = policy.getPremium().getAmount();
 
-        Ticket ticket = new Ticket(2, LocalDate.now(), 10.0, 1);
-        ticketManagement.createTicketForCustomer(1, ticket);
+        Ticket ticket = new Ticket(2, LocalDate.now(), 10.0, 101);
+        ticketManagement.createTicketForCustomer(101, ticket);
 
-        assertEquals(customerRepository.findById(1).get().getPolicies().get(0).getPremium().getAmount(), premium + 10.0);        
+        assertEquals(customerRepository.findById(101).get().getPolicies().get(0).getPremium().getAmount(), premium + ticketManagement.INCREASE_PREMIUM);        
     }
 
     
