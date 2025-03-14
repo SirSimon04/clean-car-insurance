18a713665a859cbaaeead7b04785788d18e5752d
SirSimon04 <simi@engelnetz.de>
2025-03-14 22:19:25 +0100

    refactor(srv): Refactor premium calculation strategy

--- 2-insurance-application/src/main/java/de/sri/application/premiumcalculator/PremiumCalculationStrategyFactory.java
+++ 2-insurance-application/src/main/java/de/sri/application/premiumcalculator/PremiumCalculationStrategyFactory.java
@@ -2,17 +2,23 @@ package de.sri.application.premiumcalculator;
 
 import de.sri.domain.entities.PolicyProgram;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class PremiumCalculationStrategyFactory {
+    private static final Map<PolicyProgram, PremiumCalculationStrategy> strategies = new HashMap<>();
+
+    static {
+        strategies.put(PolicyProgram.BASIC, new BasicPremiumCalculationStrategy());
+        strategies.put(PolicyProgram.STANDARD, new StandardPremiumCalculationStrategy());
+        strategies.put(PolicyProgram.DELUXE, new DeluxePremiumCalculationStrategy());
+    }
+
     public static PremiumCalculationStrategy getStrategy(PolicyProgram program) {
-        switch (program) {
-            case BASIC:
-                return new BasicPremiumCalculationStrategy();
-            case STANDARD:
-                return new StandardPremiumCalculationStrategy();
-            case DELUXE:
-                return new DeluxePremiumCalculationStrategy();
-            default:
-                throw new IllegalArgumentException("Unknown policy program: " + program);
+        PremiumCalculationStrategy strategy = strategies.get(program);
+        if (strategy == null) {
+            throw new IllegalArgumentException("Unknown policy program: " + program);
         }
+        return strategy;
     }
 }
